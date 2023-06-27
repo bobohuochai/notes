644b8983abf2e31d15a9a835095b7a49bc2ba744
徐军 <xujun@pingpongx.com>
2023-06-27 19:20:41 +0800

    feat: + 移动端适配方案指南

--- learn/prototype.js
+++ learn/prototype.js
@@ -4,13 +4,16 @@ function Super(name) {
 Super.prototype.add = function() {
    this.age = this.age + 1
 }
-Super.prototype.age =1
+Super.prototype.age =1 
+Super.prototype.data = {
+    count:1
+}
 
 
 const a = new Super('A')
 const b = new Super('B')
-a.add()
-console.log(b.age,a.age)
+a.data.count =2
+console.log(b.data,a.data)
 
 
 
